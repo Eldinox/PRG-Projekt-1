e8d0e2f575f44a1b203f14317d92ee17e1873bcb
Eldinox <waldemar.g94@web.de>
2016-10-02 14:38:20 +0200

    Fehler nach Feedback behoben

--- Gruppen_de/mainMenuButtons.php
+++ Gruppen_de/mainMenuButtons.php
@@ -1,11 +1,10 @@
-<!-- Buttons die als Hauptmenü in Index.html included werden, wenn man eingeloggt ist.		
-		Im Moment hat nur der Logout Button eine Funktion, die anderen reloaden bloß die Seite -->
+<!-- Buttons die als Hauptmenü in InMenu.html included werden. -->
 <?php
-	echo "<form action='Index.html'><button>Spiel starten</button></form>";
-	echo "<form action='Index.html'><button>Optionen</button></form>";
-	echo "<form action='Index.html'><button>Hilfe</button></form>";
-	echo "<form action='Index.html'><button>Highscore</button></form>";
-	echo "<form action='Gruppen_de/logout.php'><button>Ausloggen</button></form>";
+	echo 	"<form method='post' action='InMenu.html?modeselection'><button>Spiel starten</button></form>
+			<form method='post' action='InMenu.html?options'><button>Optionen</button></form>
+			<form method='post' action='InMenu.html?help'><button>Hilfe</button></form>
+			<form method='post' action='InMenu.html?highscore'><button>Highscore</button></form>
+			<form action='Gruppen_de/logout.php'><button>Ausloggen</button></form>";
 ?>
 
 <!--
