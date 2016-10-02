f664295670ae6c0ab97ead16ce903be2e76dbb92
Eldinox <waldemar.g94@web.de>
2016-10-02 14:36:22 +0200

    kleine Änderung am Button

--- Gruppen_de/helpmenu.php
+++ Gruppen_de/helpmenu.php
@@ -2,6 +2,6 @@
 	echo "<article id='help'>
 		<p><h2>Anleitung zum Spiel</h2></p>
 		<p>Hier erfahren Anfänger, wie das Spiel gespielt wird</p>
-		<form action='Index.html'><button>Zur&uuml;ck zum Hauptmen&uuml;</button></form>
+		<form method='post' action='InMenu.html?mainmenu'><button>Zur&uuml;ck zum Hauptmen&uuml;</button></form>
 		</article>"
 ?>
